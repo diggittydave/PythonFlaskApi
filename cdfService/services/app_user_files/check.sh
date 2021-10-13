0480471d7bd8a2265a0da76dd15396ae1e16c20c
diggittydave <78184755+diggittydave@users.noreply.github.com>
2021-10-12 22:02:33 -0700

    Delete cdfService/services directory

--- cdfService/services/app_user_files/check.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-echo 'cdfsvc is running with the following processes'
-echo ''
-ps -fu $app_user --forest|grep "gunicorn"|grep -v grep
-echo ''
-echo ''
-echo 'SVN UPDATE WORKER running with the following process'
-ps -fu $app_user --forest|grep "svnUpdate"|grep -v grep
-echo ''
