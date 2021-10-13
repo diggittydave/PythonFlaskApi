f6c12c219af18e15f7fe4d15f3163597b40aaaa9
diggittydave <78184755+diggittydave@users.noreply.github.com>
2021-10-12 18:43:52 -0700

    Update config.py

--- cdfService/config.py
+++ cdfService/config.py
@@ -7,15 +7,13 @@ transport = Transport(timeout=10)
 
 
 class SnPClients:
-    qa_client = Client('http://qaspcentral.chq.ei:8121/spsoap/services/SPWebService?wsdl', transport=transport)
-    dev_client = Client('http://devspcentral.chq.ei:8121/spsoap/services/SPWebService?wsdl', transport=transport)
-    prod_client = Client('http://spcentral.chq.ei:8121/spsoap/services/SPWebService?wsdl', transport=transport)
+    #Deleted contents for security purposes.
 
 
 
 class ConfigClass:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = 'exch-smtp.expeditors.com'
+    MAIL_SERVER = #delete for security purposes.
     MAIL_PORT = 25
     MAIL_USE_TLS = False
     MAIL_DEBUG = True
@@ -32,35 +30,13 @@ class ConfigClass:
         'CA_CERTS': os.environ.get('CA_CERTS'),
         'DEBUG': True
         }
-    basedir = os.path.abspath(r'/prod/svc/cdfsvc/cdfservice/cdfService/database/CDF_INFO.sqlite')
-    appdir = os.path.abspath(r'/prod/svc/cdfsvcqa/cdfservice')  # applicaiton direcorty location.
+    basedir = os.path.abspath() #delete for security
+    appdir = os.path.abspath()  # applicaiton direcorty location.
     repo_path = os.path.abspath(r'/prod/svc/cdfsvc/cdfservice/repository')  # primary repository paths.
     svn_folders = [repo_path + '/cdfs/cdfs', repo_path + '/prod/prod', repo_path + '/svcp_prod/svcp_prod']  # separated repository paths.
-    error_email_address = 'christopher.sharratt@expeditors.com'  # support email group
-    dev_error_email_address = 'david.weber@expeditors.com'  # dev email group
+    error_email_address =  'support email group'
+    dev_error_email_address =   'dev email group'
     SNP_Client = SnPClients.prod_client  # set for prod/qa/dev sn
-
-
-class Config_QA:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = 'exch-smtp.expeditors.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # elastic configurations
-    ELASTIC_APM = {
-        'SERVICE_NAME': 'CDF_Service',
-        'SECRET_TOKEN': os.environ.get('SECRET_TOKEN'),
-        'SEVER_URL': os.environ.get('SERVER_URL'),
-        'SERVER_CERT': os.environ.get('SERVER_CERT'),
-        'CA_CERTS': os.environ.get('CA_CERTS'),
-        'DEBUG': True
-        }
-    basedir = os.path.abspath(r'/prod/svc/cdfsvcqa/cdfservice/cdfService/database/CDF_INFO.sqlite')
-    appdir = os.path.abspath(r'/prod/svc/cdfsvcqa/cdfservice/')
-    repo_path = os.path.abspath(r'/prod/svc/cdfsvcqa/cdfservice/repository')
-    svn_folders = [repo_path + '/cdfs/cdfs', repo_path + '/prod/prod']
 
 
 class Config_Swagger_UI:
